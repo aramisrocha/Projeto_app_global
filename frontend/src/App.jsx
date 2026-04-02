4e6d1b7440e4e81ad5d24fe82a8fd2187f1f8e4d
Aramis de Oliveira Andrade Rocha <aramis.rocha@ativas.com.br>
2026-04-02 18:51:33 -0300

    Tshhot cognito

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -157,8 +157,10 @@ export default function App() {
       if (!token) {
         throw new Error("Usuário administrativo não autenticado.");
       }
-      console.log("API_BASE_URL:", API_BASE_URL);
-      console.log("URL final:", `${API_BASE_URL}/employees`);
+      const url = `${API_BASE_URL}/employees`;
+
+      console.log("API_BASE_URL =", API_BASE_URL);
+      console.log("URL final =", url);
       const res = await fetch(`${API_BASE_URL}/employees`, {
         method: "POST",
         headers: {
